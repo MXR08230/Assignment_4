fa33a8f32a5594428153ba3433769a36b7acc605
Mounika <MXR08230@ucmo.edu>
2022-11-03 23:13:00 -0500

    updated

--- question1.py
+++ question1.py
@@ -14,18 +14,23 @@ import seaborn as sns
 sns.set(style="white", color_codes=True)
 import warnings
 warnings.filterwarnings("ignore")
+#Importing the Salary_Data csv file
 df=pd.read_csv("./Salary_Data.csv")
 print(df.head())
+
+#Slicing the features using iloc
 X = df.iloc[:, :-1].values
 Y = df.iloc[:, 1].values
 X_Training, X_Testing, Y_Training, Y_Testing = train_test_split(X,Y, test_size=1/3,random_state = 0)
 
+#Training and Predicting the model
 regressor = LinearRegression()
 regressor.fit(X_Training, Y_Training)
-
 Y_Predict = regressor.predict(X_Testing)
 
+#calculating mean_squared_error
 print(mean_squared_error(Y_Testing,Y_Predict))
+
 #Scatterplot for Training Data
 plt.title('Training data')
 plt.xlabel('Years of Experience')
--- question2.py
+++ question2.py
@@ -1,27 +1,23 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
 from sklearn import preprocessing
-from sklearn.metrics import mean_squared_error
 from sklearn.cluster import KMeans
 from sklearn.impute import SimpleImputer
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-import seaborn as sns
+
+#To display all columns in the output
 desired_width=320
 pd.set_option('display.width', desired_width)
 pd.set_option('display.max_columns',20)
+
 df2=pd.read_csv("./K-Mean_Dataset.csv")
 print(df2.head())
 X = df2.iloc[:,1:].values
-
+#Removing Null values using "Mean"
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer = imputer.fit(X)
 x = imputer.transform(X)
-
+#Using the elbow method to find a good number of clusters with the K-Means algorithm
 wcss = []
 for i in range(1,11):
     kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
@@ -33,13 +29,32 @@ plt.title('the elbow method')
 plt.xlabel('Number of Clusters')
 plt.ylabel('Wcss')
 plt.show()
-
+#Implementing K means clustering
 from sklearn.cluster import KMeans
 nclusters = 2
 km = KMeans(n_clusters=nclusters)
 print(km.fit(x))
-
+#Calculating the silhouette score
 y_cluster_kmeans = km.predict(x)
 from sklearn import metrics
 score = metrics.silhouette_score(x, y_cluster_kmeans)
 print('Silhouette score:',score)
+
+#question 3
+#Try feature scaling and then apply K-Means on the scaled features. Did that improve the Silhouette score? If Yes, can you justify why
+
+scaler = preprocessing.StandardScaler()
+scaler.fit(x)
+X_scaled_array = scaler.transform(x)
+X_scaled = pd.DataFrame(X_scaled_array)
+
+from sklearn.cluster import KMeans
+nclusters = 2
+km = KMeans(n_clusters=nclusters)
+print(km.fit(X_scaled))
+
+#calcualating Silhouette score after applying scaling
+y_scaled_cluster_kmeans = km.predict(X_scaled)
+from sklearn import metrics
+score = metrics.silhouette_score(X_scaled, y_scaled_cluster_kmeans)
+print('Silhouette score after applying scaling:',score)
